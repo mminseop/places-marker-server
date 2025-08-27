9fce28f219280b8866e87d5539be897277c3f044
mminseop <alstjq903@naver.com>
2025-08-28 02:10:45 +0900

    ⚡️ Feat: 구글 플레이스 통해 데이터 응답 완료

--- routes/places.js
+++ routes/places.js
@@ -4,21 +4,21 @@ import axios from "axios";
 const router = express.Router();
 
 router.get("/search", async (req, res) => {
-  console.log("Request received:", req.query.query);
   const { query } = req.query;
   const apiKey = process.env.GOOGLE_PLACE_KEY;
-  console.log("api Key:", apiKey);
 
   try {
     console.log("query:", query);
-    const res = await axios.get(
-      `https://maps.googleapis.com/maps/api/place/textsearch/json`,
+
+    const response = await axios.get(
+      "https://maps.googleapis.com/maps/api/place/textsearch/json",
       { params: { query, key: apiKey } }
     );
-    console.log("google aip res:", res.data);
-    res.json(res.data);
+
+    console.log("Google API response:", response.data);
+    res.json(response.data); // Express res 객체 사용
   } catch (e) {
-    console.eor("Google API call failed:", e.response?.data || err.message);
+    console.error("Google API call failed:", e.response?.data || e.message);
     res.status(500).json({ error: "API 호출 에러" });
   }
 });
