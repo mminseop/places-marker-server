69963cc1bb58a616f4383d8b7d3b8da9000ee3a7
mminseop <alstjq903@naver.com>
2025-08-29 18:37:18 +0900

    🐛 Fix: db오류 수정2

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const db = mysql.createPool({
+const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
@@ -11,3 +11,5 @@ export const db = mysql.createPool({
   waitForConnections: true,
   connectionLimit: 10,
 });
+
+export default db;
